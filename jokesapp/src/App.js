1e6c3352ce8bb07b9b9c6022389c8eafed105f8b
Laura Helena Cabra Acela <lh.cabra@uniandes.edu.co>
2021-10-30 18:29:25 -0500

    taller Jokes

---
+++ jokesapp/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Joke from './components/Joke'
+
+
+function App() {
+  return (
+    <Joke/>
+  );
+}
+
+export default App;
---
+++ jokesapp/src/components/Joke.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+
+export default function Joke() {
+  const [joke, setJoke] = useState("");
+
+  useEffect(()=>{
+    if(!navigator.onLine){
+        if(localStorage.getItem("joke") === null) {
+            setJoke("Loading...")
+        } else {
+            setJoke(localStorage.getItem("joke"));
+        }
+    } else {
+        const URL = "https://api.chucknorris.io/jokes/random";
+        fetch(URL).then(res=>res.json()).then(res=>{
+            setJoke(res.value);
+            localStorage.setItem("joke", res.value);
+        })
+    }
+}, []);
+
+  return <h1>{joke}</h1>;
+}
